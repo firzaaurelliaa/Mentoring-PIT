876a9591dd713d57b670b50d02a560ffe34a573f
firzaurellia <firzaurellia@gmail.com>
2023-11-01 19:09:23 +0700

    perubahan di landing

--- src/Views/Landing.jsx
+++ src/Views/Landing.jsx
@@ -2,7 +2,7 @@ const Landing = () => {
     return (
         <>
             <div className="flex">
-                <div>ini Landing</div>
+                <div>ini Landing s</div>
                 <div>landing 2</div>
             </div>
         </>
